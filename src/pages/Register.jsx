966534cc0da769a7762c79171588b62088a71b4d
Dinesh Rout <rddinesh779@gmail.com>
2024-02-03 21:09:00 +0530

    validations ✅

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,12 +11,14 @@ const Register = () => {
     email: '',
     password: '',
     phone: '',
-    gender: '',
+    gender: 'male',
     country: '',
-    state: '',
+    stateName: '',
     city: '',
   });
-  const { email, fullName, password } = values;
+  const { email, fullName, password, phone, gender, country, stateName, city } =
+    values;
+  const [error, setError] = useState(null);
 
   const handleInputChange = (e) => {
     setValues({
@@ -29,9 +31,25 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      dispatch(
-        register({ id: Math.floor(Math.floor(Math.random() * 11)), ...values })
-      );
+      if (
+        email &&
+        fullName &&
+        password &&
+        phone &&
+        gender &&
+        country &&
+        stateName &&
+        city
+      ) {
+        dispatch(
+          register({
+            id: Math.floor(Math.floor(Math.random() * 11)),
+            ...values,
+          })
+        );
+      } else {
+        setError('All fields are required!');
+      }
     } catch (error) {
       console.log(error);
     }
@@ -47,10 +65,11 @@ const Register = () => {
     <div className='flex justify-center items-center flex-col min-h-screen'>
       <form
         onSubmit={handleFormsubmit}
-        className='w-[90%] p-8 shadow-xl border rounded-lg max-w-[480px] mx-auto'
+        className='w-[90%] p-4 my-8 px-8 shadow-xl border rounded-lg max-w-[480px] mx-auto'
       >
         <h1 className='text-2xl font-semibold my-4'>Register to get started</h1>
 
+        {/* full name */}
         <div className='mb-4'>
           <label htmlFor='fullName'>Full name</label>
           <input
@@ -63,6 +82,8 @@ const Register = () => {
             className='block w-full my-2 p-2 border border-gray-300 rounded-md'
           />
         </div>
+
+        {/* email */}
         <div className='mb-4'>
           <label htmlFor='email'>Email</label>
           <input
@@ -75,6 +96,8 @@ const Register = () => {
             className='block w-full my-2 p-2 border border-gray-300 rounded-md'
           />
         </div>
+
+        {/* password */}
         <div className='mb-4'>
           <label htmlFor='password'>Password</label>
           <input
@@ -87,6 +110,77 @@ const Register = () => {
             className='block w-full my-2 p-2 border border-gray-300 rounded-md'
           />
         </div>
+
+        {/* phone */}
+        <div className='mb-4'>
+          <label htmlFor='phone'>Phone number</label>
+          <input
+            type='text'
+            placeholder='Enter phone number'
+            name='phone'
+            id='phone'
+            value={phone}
+            onChange={handleInputChange}
+            className='block w-full my-2 p-2 border border-gray-300 rounded-md'
+          />
+        </div>
+
+        {/* gender */}
+        <div className='mb-4'>
+          <label htmlFor='gender'>Gender</label>
+          <select
+            name='gender'
+            id='gender'
+            value={gender}
+            onChange={handleInputChange}
+            className='block w-full my-2 p-2 border border-gray-300 rounded-md'
+          >
+            <option value='male'>Male</option>
+            <option value='female'>Female</option>
+          </select>
+        </div>
+
+        {/* country */}
+        <div className='mb-4'>
+          <label htmlFor='country'>Country</label>
+          <input
+            type='text'
+            placeholder='Enter country'
+            name='country'
+            id='country'
+            value={country}
+            onChange={handleInputChange}
+            className='block w-full my-2 p-2 border border-gray-300 rounded-md'
+          />
+        </div>
+
+        {/* state */}
+        <div className='mb-4'>
+          <label htmlFor='stateName'>State</label>
+          <input
+            type='text'
+            placeholder='Enter state'
+            name='stateName'
+            id='stateName'
+            value={stateName}
+            onChange={handleInputChange}
+            className='block w-full my-2 p-2 border border-gray-300 rounded-md'
+          />
+        </div>
+
+        {/* city */}
+        <div className='mb-4'>
+          <label htmlFor='city'>City</label>
+          <input
+            type='text'
+            placeholder='Enter city'
+            name='city'
+            id='city'
+            value={city}
+            onChange={handleInputChange}
+            className='block w-full my-2 p-2 border border-gray-300 rounded-md'
+          />
+        </div>
         <div className='mb-4'>
           <button
             type='submit'
@@ -95,6 +189,8 @@ const Register = () => {
             Register
           </button>
         </div>
+
+        <p className='text-center text-red-500'>{error ? error : ''}</p>
       </form>
     </div>
   );
